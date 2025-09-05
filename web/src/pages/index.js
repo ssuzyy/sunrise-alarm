7198fa8b90db165297ca4068457ca27ea9c99d9e
Suzanne <swesuzy@gmail.com>
2025-09-04 21:48:29 -0400

    added buttons to test state/connected persistor

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,14 +1,37 @@
-import Head from 'next/head';
-import styles from '@/styles/Home.module.css';
+import { useSelector, useDispatch } from 'react-redux'
+import { setColor, setBrightness } from 'store/actions/alarm'
 
 export default function Home() {
+
+  const color = useSelector(state => state.alarm.color)
+  const brightness = useSelector(state => state.alarm.brightness)
+
+  const dispatch = useDispatch()
+
   return (
     <>
-      <Head>
-        <title>Sunrise Alarm Setup</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <h1>hiiiii :3</h1>
+      
+      <p>Color: {color}</p>
+      <p>Brightness: {brightness}</p>
+      
+      <button onClick={() => dispatch(setColor('#ff0000'))}>
+        Set Color (Red)
+      </button>
+
+      <button onClick={() => dispatch(setColor('#ffffff'))}>
+        Set Color (White)
+      </button>
+      
+      <button onClick={() => dispatch(setBrightness(75))}>
+        Set Brightness (75%)
+      </button>
+
+      <button onClick={() => dispatch(setBrightness(50))}>
+        Set Brightness (50%)
+      </button>
+      
+      <p>Refresh the page and the values should persist ! godwilling ...</p>
     </>
   );
 }
